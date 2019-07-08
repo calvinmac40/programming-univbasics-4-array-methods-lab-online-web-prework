83bb969aab717c2d84a329959ff11cec5ee11725
Calvin McDowell <calvinmac40@hotmail.com>
2019-07-08 22:07:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def using_include(array, element)
   "this_is_an #{array} and an #{element}." 
    big_money_team = ["Big", "Bank", "Take", "Lil Bank"]
  end 
- p using_include("Array","Element")
+ 
  p big_money_team.include?("Big")
 
 def using_sort(array)
